b8c0449f6dc3657261d5a09a394534fee7ece5b4
PETROMYZONMONSTER <PETROMYZONMONSTER@gmail.com>
2024-06-06 20:38:29 -0300

    PUSH

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,16 @@ import HOME from "../views/HOME.vue";
 import WELCOME from "../views/WELCOME.vue";
 import LEARNMORE from "../views/LEARNMORE.vue";
 import LISTS from "../views/LISTS.vue";
-
+import ACCOUNT from "../views/ACCOUNT.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/welcome',
+      path: '/',
       component: WELCOME
     },
     {
-      path: '/',
+      path: '/home',
       component: HOME
     },
     {
@@ -22,6 +22,10 @@ const router = createRouter({
     {
       path: '/lists',
       component: LISTS
+    },
+    {
+      path: '/login',
+      component: ACCOUNT
     }
   ]
 })
--- src/views/WELCOME.vue
+++ src/views/WELCOME.vue
@@ -4,20 +4,54 @@
 
 <template>
 
-  <div>
-    <p>
-      ;-(
-    </p>
-  </div>
-
-  <div>
-    <p>
-      ;-)
-    </p>
+<div>
+    <div>
+      <u>SIGN IN</u> <u>CREATE ACCOUNT</u> <U>FILMS</U>
+    </div>
+    <div class="body">
+      <h1>
+        Your superhero hub.
+      </h1>
+      <h2>
+        Keep in touch with your favorite characters, track their stories and continuity - all in one site.
+      </h2>
+      <RouterLink to="/login"><button type="reset" class="register-button">JOIN FOR FREE</button></RouterLink>
+        </div>
+    <div>
+      <RouterLink to="/more"><u>LEARN MORE</u></RouterLink>
+    </div>
   </div>
 
 </template>
 
 <style scoped>
+
+h1 {
+  margin-top: 100px;
+  margin-bottom: 10px;
+  font-size: 3rem;
+  font-weight: bolder;
+}
+
+h2 {
+
+  font-size: 2rem;
+  font-weight: lighter;
+  width: 500px;
+
+}
+
+.register-button {
+  background-color: #FF914D;
+  border: 1px;
+  border-radius: 50px;
+  color: #000000;
+  cursor: pointer;
+  font-size:1.2rem;
+  font-weight: lighter;
+  width: 200px;
+  height: 60px;
+  margin-bottom: -50px;
+}
 
 </style>
